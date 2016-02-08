bf03df6cb0c86880b438c71e2fb7ffb04491c170
chijr <chijr@139.com>
2016-02-08 18:03:47 +0800

    1.增加了摇摇手势框架 2.增加了摇摇的策略

---
+++ coupon/service/ShakeService.h
@@ -0,0 +1,13 @@
+//
+//  ShakeService.h
+//  coupon
+//
+//  Created by chijr on 16/2/8.
+//  Copyright (c) 2016年 chijr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShakeService : NSObject
+
+@end
---
+++ coupon/service/ShakeService.m
@@ -0,0 +1,13 @@
+//
+//  ShakeService.m
+//  coupon
+//
+//  Created by chijr on 16/2/8.
+//  Copyright (c) 2016年 chijr. All rights reserved.
+//
+
+#import "ShakeService.h"
+
+@implementation ShakeService
+
+@end
