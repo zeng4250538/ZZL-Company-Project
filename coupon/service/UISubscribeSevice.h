687a1d407b2ef8b7a182b3abd1688cca5bf04845
zengzhaolin <zengzhaolin@richstonedt.com>
2016-03-29 20:00:29 +0800

    更新：前后端整合

---
+++ coupon/service/UISubscribeSevice.h
@@ -0,0 +1,13 @@
+//
+//  UISubscribeSevice.h
+//  coupon
+//
+//  Created by ZZL on 16/3/29.
+//  Copyright © 2016年 chijr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UISubscribeSevice : NSObject
+
+@end
---
+++ coupon/service/UISubscribeSevice.m
@@ -0,0 +1,13 @@
+//
+//  UISubscribeSevice.m
+//  coupon
+//
+//  Created by ZZL on 16/3/29.
+//  Copyright © 2016年 chijr. All rights reserved.
+//
+
+#import "UISubscribeSevice.h"
+
+@implementation UISubscribeSevice
+
+@end
